82409029c6c97f5b6060befbf78dfacbb03a8bb9
unknown <satya-1298>
2023-04-24 22:45:29 +0530

    Table Creation with attributes

--- AddressBook/AdressBookService.sql
+++ AddressBook/AdressBookService.sql
@@ -1,3 +1,16 @@
 /********************Welcome to AddressBook Database****************/
 /***************UC1**************/
 create database AddressBookService
+
+/**************UC2***************/
+create table AddressBook
+(
+FirstName varchar(20),
+LastName Varchar(20),
+Address varchar(225),
+City varchar(20),
+State varchar(20),
+Zip int,
+PhoneNumber varchar(20),
+Email varchar(220)
+)
